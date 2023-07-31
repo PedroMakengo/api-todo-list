b1c2370e8bd67d1c1e7c54cedc58ce4ce127a5b6
PedroMakengo <pedromakengomiguel@gmail.com>
2023-07-31 06:54:10 +0100

    Adicionando novas atualizações

---
+++ SistemaDeTarefas/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        Task<List<TarefaModel>> BuscarTodasTarefas();
+        Task<TarefaModel> BuscarPorId(int id);
+        Task<TarefaModel> Adicionar(UsuarioModel tarefa);
+        Task<TarefaModel> Atualizar(UsuarioModel tarefa, int id);
+        Task<bool> Apagar(int id);
+    }
+}
